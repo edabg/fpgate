5623f43e4eab85160fa92aa4d32b6fa5a9e6987f
Dimitar Angelov <mitko@edabg.com>
2015-07-17 15:15:21 +0300

    Add license

--- FPGateSrv/src/main/java/org/eda/fpsrv/FPDatabase.java
+++ FPGateSrv/src/main/java/org/eda/fpsrv/FPDatabase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Nikola Bintev
+ * Copyright (C) 2015 EDA Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
