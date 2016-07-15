d084b02a6b4db052925cafca6d1e25464939fe16
Dimitar Angelov <mitko@edabg.com>
2016-07-15 14:49:14 +0300

    Add support for Datecs FP705KL

---
+++ FPGateSrv/src/main/java/org/datecs/fpcom/CS_BGR_FMP350_KL.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package org.datecs.fpcom;
+
+import com.jacob.com.*;
+
+public class CS_BGR_FMP350_KL extends ICS_BGR_FMP350_KL {
+
+	public static final String componentName = "clsid:{6756A725-F113-4F6C-8174-110561106C6E}";
+
+	public CS_BGR_FMP350_KL() {
+		super(componentName);
+	}
+
+	public CS_BGR_FMP350_KL(Dispatch d) {
+		super(d);
+	}
+}
